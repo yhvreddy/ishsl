0f6e4b61788ac77f6dc9e8b5cd7945bfc5ced72e
yhvreddy <yhvreddyinfo@gmail.com>
2020-04-03 15:35:58 +0530

    init master_data files.

---
+++ locations/cities.php
@@ -0,0 +1,37 @@
+<?php include '../layouts/header.php'; ?>
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-primary">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title mt-1 mb-1">Add Country</h4>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title mt-1 mb-1">Countries List</h4>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <!-- content-wrapper ends -->
+<?php include '../layouts/footer.php'; ?>  
