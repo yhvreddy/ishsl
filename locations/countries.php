3b7903b8913ac8703aea7ae84409f7d3f710ea34
yhvreddy <yhvreddyinfo@gmail.com>
2020-04-03 19:05:40 +0530

    updated master_data files.

--- locations/countries.php
+++ locations/countries.php
@@ -5,7 +5,8 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-primary">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+                    <li class="breadcrumb-item"><a href="#">Locations</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Countries</li>
                     </ol>
                 </nav>
             </div>
@@ -16,7 +17,23 @@
                         <h4 class="card-title mt-1 mb-1">Add Country</h4>
                     </div>
                     <div class="card-body">
-
+                        <form method="post" action="#">
+                            <div class="form-group">
+                                <label for="conuntryName">Country Name</label>
+                                <input type="text" placeholder="Enter country name" name="countryName" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="countryShortName">Country Short Name</label>
+                                <input type="text" placeholder="Enter country short name" name="countryShortName" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="countryCode">Country code</label>
+                                <input type="text" placeholder="Enter country code" name="countryCode" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-success" value="save country">
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -27,7 +44,31 @@
                         <h4 class="card-title mt-1 mb-1">Countries List</h4>
                     </div>
                     <div class="card-body">
-
+                        <div class="table-responsive">
+                            <table id="order-listing" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr class="bg-primary text-white">
+                                        <th>#</th>
+                                        <th>Country Name</th>
+                                        <th>Short Name</th>
+                                        <th>Code</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>India</td>
+                                        <td>INR</td>
+                                        <td>+91</td>
+                                        <td align="center">
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Edit"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-grease-pencil"></i></a></span>
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Delete"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-delete"></i></a></span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
--- roles/roles.php
+++ roles/roles.php
@@ -5,7 +5,7 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-primary">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+                    <li class="breadcrumb-item active" aria-current="page">Roles types</li>
                     </ol>
                 </nav>
             </div>
@@ -13,10 +13,19 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title mt-1 mb-1">Add Country</h4>
+                        <h4 class="card-title mt-1 mb-1">Add Role Type</h4>
                     </div>
                     <div class="card-body">
-
+                        <form method="post" action="#">
+                            <div class="form-group">
+                                <label for="roleType">Role type</label>
+                                <input type="text" placeholder="Enter role type" name="roleType" class="form-control" required>
+                            </div>
+                            
+                            <div class="form-group text-right">
+                                <input type="submit" class="btn btn-success" value="save role type">
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -24,10 +33,46 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title mt-1 mb-1">Countries List</h4>
+                        <h4 class="card-title mt-1 mb-1">Role Types List</h4>
                     </div>
                     <div class="card-body">
-
+                        <div class="table-responsive">
+                            <table id="order-listing" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr class="bg-primary text-white">
+                                        <th>#</th>
+                                        <th>Role Types</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Super-admin</td>
+                                        <td align="center">
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Edit"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-grease-pencil"></i></a></span>
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Delete"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-delete"></i></a></span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Admin</td>
+                                        <td align="center">
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Edit"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-grease-pencil"></i></a></span>
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Delete"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-delete"></i></a></span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Hospital</td>
+                                        <td align="center">
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Edit"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-grease-pencil"></i></a></span>
+                                            <span data-toggle="tooltip" data-placement="top" data-original-title="Delete"><a href="#" class="mr-1 text-muted p-2"><i class="mdi mdi-delete"></i></a></span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
